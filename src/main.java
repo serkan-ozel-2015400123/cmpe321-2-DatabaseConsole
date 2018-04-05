9fcb95225e29bee5785bfcd3a6dfa22e9110c731
serkan-ozel-2015400123 <serkan.ozel@boun.edu.tr>
2018-04-05 14:45:26 +0300

    add v1 of list all types

--- src/main.java
+++ src/main.java
@@ -25,7 +25,7 @@ public class main {
 		for(int i =0;i<160000;i++) {
 			if(i%1600 ==0) {
 				systemCatFile.writeInt(5);
-			}else if(i%1600>=5) {
+			}else if(i%1600>=4) {
 				systemCatFile.write(0);
 			}
 			
@@ -57,9 +57,9 @@ public class main {
 			System.out.println("Enter a number to continue...");
 			try{
 
-String input = user.next();
+				String input = user.next();
 				switch(input) {
-				case "1":
+				case "1": ////////////////////////////// CREATE A TYPE///////////////////////
 
 					System.out.println("Enter name:");
 					System.out.println("It should be alphanumeric. It should not contain non-ascii characters. e.g some turkish letters. ");
@@ -105,7 +105,8 @@ String input = user.next();
 					System.out.println("Created type: " + typeName + " with " + entered + " field(s).");
 					System.out.println("---------------------------------------------------------------");
 					break;
-				case "2": // Look for the given type and if found delete it. Also need to shift afterwards.
+				case "2": ////////////////////////////////DELETE A TYPE/////////////////////////// 
+					// Look for the given type and if found delete it. Also need to shift afterwards.
 					System.out.println("Please enter the name of the type you want to delete....");
 					String typeNamed = user.next();
 					int loc = 0;
@@ -197,7 +198,112 @@ String input = user.next();
 
 					
 					break;
-				case "3":
+				case "3": //////////////////////////LIST ALL TYPES////////////////////////////
+					
+					systemCatFile.seek(4);
+					int numFulll =  systemCatFile.read();
+					
+					for(int page=0;page<numFulll; page++) {
+						System.out.println("Reading page " + (page+1) +  "..");
+						
+						if(page ==0) { 
+							int totalRead = 5;
+							while(totalRead <1600) { // When we finish page
+								byte[] typename = new byte[32];
+								
+								systemCatFile.seek(totalRead);
+								int full = systemCatFile.read(typename,0,32);
+								if(full==-1)break;
+								
+								
+								String typename_ = new String(typename,StandardCharsets.UTF_8);
+								
+								String typenameNew_="";
+								for(int i=0;i<typename_.length();i++) {
+									if(typename_.charAt(i)!=0)typenameNew_ = typenameNew_+typename_.charAt(i);
+								}
+								
+								
+								
+
+								int fieldNum=systemCatFile.read();
+								if(fieldNum==-1)break;
+								if(typenameNew_.length()!=0)System.out.println("Typename:'"+ typenameNew_ +"' with "+fieldNum+" fields.");
+								for(int i=1;i<=fieldNum;i++) {
+									byte[] fieldName = new byte[16];
+									
+									systemCatFile.read(fieldName,0,16);
+									
+									String fieldname_ = new String(fieldName,StandardCharsets.UTF_8);
+									
+									String fieldnameNew_="";
+									for(int j=0;j<fieldname_.length();j++) {
+										if(fieldname_.charAt(j)!=0)fieldnameNew_ = fieldnameNew_+fieldname_.charAt(j);
+									}
+									
+									
+									
+									if(fieldnameNew_.length()!=0)System.out.print("Field" +i + " Name:" + fieldnameNew_ + " ");
+								}
+								if(typenameNew_.length()!=0)System.out.println("");
+								totalRead = totalRead +32+1 +fieldNum*16; // +1 is for the fieldNum byte. totalRead is previously read position. And we have also fieldNum*16 bytes to advance.
+							}
+
+
+
+						}else{ 
+							int totalRead = 4;
+							while(totalRead <1600) { // When we finish page
+								byte[] typename = new byte[32];
+								
+								systemCatFile.seek(totalRead);
+								int full = systemCatFile.read(typename,0,32);
+								if(full==-1)break;
+								
+								
+								String typename_ = new String(typename,StandardCharsets.UTF_8);
+								
+								String typenameNew_="";
+								for(int i=0;i<typename_.length();i++) {
+									if(typename_.charAt(i)!=0)typenameNew_ = typenameNew_+typename_.charAt(i);
+								}
+								
+								
+								
+
+								int fieldNum=systemCatFile.read();
+								if(fieldNum==-1)break;
+								if(typenameNew_.length()!=0)System.out.println("Typename:'"+ typenameNew_ +"' with "+fieldNum+" fields.");
+								for(int i=1;i<=fieldNum;i++) {
+									byte[] fieldName = new byte[16];
+									
+									systemCatFile.read(fieldName,0,16);
+									
+									String fieldname_ = new String(fieldName,StandardCharsets.UTF_8);
+									
+									String fieldnameNew_="";
+									for(int j=0;j<fieldname_.length();j++) {
+										if(fieldname_.charAt(j)!=0)fieldnameNew_ = fieldnameNew_+fieldname_.charAt(j);
+									}
+									
+									
+									
+									if(fieldnameNew_.length()!=0)System.out.print("Field" +i + " Name:" + fieldnameNew_ + " ");
+								}
+								System.out.println("");
+								totalRead = totalRead +32+1 +fieldNum*16; // +1 is for the fieldNum byte. totalRead is previously read position. And we have also fieldNum*16 bytes to advance.
+							}
+
+						}
+						
+					}
+					
+					
+					
+					
+					
+					
+					
 					break;
 				case "4":
 					break;
@@ -215,7 +321,7 @@ String input = user.next();
 			}catch(Exception e ) {
 				e.printStackTrace();
 				System.out.println("Invalid input. Try again.");
-
+				
 			}
 		}	
 
